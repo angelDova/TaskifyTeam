2337f24b35bc28c5cd547359eeba536f04010cff
angelDova <Angelfcb01@yahoo.com>
2024-03-06 16:12:47 -0800

    first commit

---
+++ app/(platform)/(clerk)/layout.tsx
@@ -0,0 +1,9 @@
+const ClerkLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <main className="h-screen flex items-center justify-center">
+      {children}
+    </main>
+  );
+};
+
+export default ClerkLayout;
