c5d6ece56e8434e600eb717b3908c7880c32e925
angelDova <Angelfcb01@yahoo.com>
2024-03-14 21:30:14 -0700

    modal description

--- components/modals/card-modal/header.tsx
+++ components/modals/card-modal/header.tsx
@@ -1,12 +1,15 @@
 "use client";
 
+import { updateCard } from "@/actions/update-card";
 import { FormInput } from "@/components/form/form-input";
 import { Skeleton } from "@/components/ui/skeleton";
+import { useAction } from "@/hooks/use-action";
 import { CardWithList } from "@/types";
 import { useQueryClient } from "@tanstack/react-query";
 import { Layout } from "lucide-react";
 import { useParams } from "next/navigation";
 import { ElementRef, useRef, useState } from "react";
+import { toast } from "sonner";
 
 interface HeaderProps {
   data: CardWithList;
@@ -15,6 +18,19 @@ interface HeaderProps {
 export const Header = ({ data }: HeaderProps) => {
   const queryClient = useQueryClient();
   const params = useParams();
+
+  const { execute } = useAction(updateCard, {
+    onSuccess: (data) => {
+      queryClient.invalidateQueries({
+        queryKey: ["card", data.id],
+      });
+      toast.success(`Renamed to "${data.title}"`);
+      setTitle(data.title);
+    },
+    onError(error) {
+      toast.error(error);
+    },
+  });
 
   const inputRef = useRef<ElementRef<"input">>(null);
 
@@ -25,7 +41,16 @@ export const Header = ({ data }: HeaderProps) => {
   };
 
   const onSubmit = (formData: FormData) => {
-    console.log(formData.get("title"));
+    const title = formData.get("title") as string;
+    const boardId = params.boardId as string;
+
+    if (title === data.title) return;
+
+    execute({
+      title,
+      boardId,
+      id: data.id,
+    });
   };
 
   return (
@@ -38,7 +63,7 @@ export const Header = ({ data }: HeaderProps) => {
             onBlur={onBlur}
             id="title"
             defaultValue={title}
-            className="font-semibold text-xl px-1 text-neutral-700 bg-transparent border-transparent relative -left-1.5 w-[95%] focus-visible:bg-white focus-visible:border-input mb-0.5 truncate"
+            className="font-semibold text-xl px-1  bg-transparent border-transparent relative -left-1.5 w-[95%] focus-visible:bg-white focus-visible:border-input mb-0.5 truncate"
           />
         </form>
         <p className="text-sm text-muted-foreground">
